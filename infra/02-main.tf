6cbf323dca5caf898cd26583d7afce502ccacaee
Dinh Le Nguyen <dnhlenguyen@gmail.com>
2026-05-25 16:13:35 +0200

    test scenario

--- infra/02-main.tf
+++ infra/02-main.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "demo" {
   ami           = var.ami_id
-  instance_type = var.instance_type
+  instance_type = "t3.micro"
 
   tags = {
     Name = "localstack-demo-instance"
-    env  = "dev"
+    env  = "prod"
   }
 }
